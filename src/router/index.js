65349f4b01dad075c4eb14ba0941d621a9da072e
MaryIawq <mary.iawq@gmail.com>
2024-03-05 23:28:00 +0900

    fix: [master] fixed router configuration

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Cover from "@/pages/main-page.vue";
 const routes = [
-  {path: '/', name: 'cover', component: Cover},
+  {path: '/SkyLinx/', name: 'cover', component: Cover},
 ]
 const router = createRouter({
   history: createWebHistory(),
